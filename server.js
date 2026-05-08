7bd46bf64385c4c70258eeebf949e58c2a69d845
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-05-08 16:32:50 +0530

    pm2

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const http = require('http');
 http.createServer((req, res) => {
   res.end('Hello from Node.js 👋\n');
 }).listen(8000, () => {
-  console.log('App running on port 3000');
+  console.log('App running on port 8000');
 });
