dab48e20b1ac628dad7d0b3486acf5082d94788c
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-05-08 15:55:06 +0530

    pm2

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,8 @@
-# #!/bin/bash
+#!/bin/bash
 
 export NVM_DIR="/root/.nvm"
 export PATH="$NVM_DIR/versions/node/v22.22.2/bin:/usr/local/bin:/usr/bin:/bin"
 
 PM2_CMD="/root/.nvm/versions/node/v22.22.2/bin/pm2"
 
-$PM2_CMD stop pm2_test 
-# pm2 stop pm2-test-app || true
+$PM2_CMD status >> /tmp/output.log
