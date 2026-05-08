ade093a70536424ee121e550ce2437dcf9bb9071
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-05-08 16:55:42 +0530

    pm2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,9 +5,8 @@ export PATH="$NVM_DIR/versions/node/v22.22.2/bin:/usr/local/bin:/usr/bin:/bin"
 
 PM2_CMD="/root/.nvm/versions/node/v22.22.2/bin/pm2"
 
-cd /opt/test_apps/
-mkdir pm2_test
 
-cp -r /opt/pm2_test /opt/test_apps/pm2_test 
+mv /opt/pm2_test /opt/test_apps/pm2_test 
 
-npm install --production 
+cd /opt/test_apps/pm2_test  
+npm install --production
