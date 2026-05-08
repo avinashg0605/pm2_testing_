7c00bddbd0448ca3588243cd713070e0f23de293
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-05-08 16:02:36 +0530

    pm2

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+# #!/bin/bash
 
-export NVM_DIR="/root/.nvm"
-export PATH="$NVM_DIR/versions/node/v22.22.2/bin:/usr/local/bin:/usr/bin:/bin"
+# export NVM_DIR="/root/.nvm"
+# export PATH="$NVM_DIR/versions/node/v22.22.2/bin:/usr/local/bin:/usr/bin:/bin"
 
-PM2_CMD="/root/.nvm/versions/node/v22.22.2/bin/pm2"
+# PM2_CMD="/root/.nvm/versions/node/v22.22.2/bin/pm2"
 
-$PM2_CMD status >> /tmp/output.log
+# $PM2_CMD status >> /tmp/output.log
 
 
-# Devops Engineer 
+# # Devops Engineer 
