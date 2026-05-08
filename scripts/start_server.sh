07593b52c2e75e1556636e06fcb15c5f33d814d7
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-05-08 16:28:44 +0530

    pm2

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,11 @@
 export NVM_DIR="/root/.nvm"
 export PATH="$NVM_DIR/versions/node/v22.22.2/bin:/usr/local/bin:/usr/bin:/bin"
 
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm  
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+# PM2_CMD=`which pm2`
 PM2_CMD="/root/.nvm/versions/node/v22.22.2/bin/pm2"
 
 
