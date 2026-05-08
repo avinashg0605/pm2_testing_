a7af0419cd80562248131acb63cb8e20330bd712
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-05-08 17:04:04 +0530

    pm2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,8 +5,15 @@ export PATH="$NVM_DIR/versions/node/v22.22.2/bin:/usr/local/bin:/usr/bin:/bin"
 
 PM2_CMD="/root/.nvm/versions/node/v22.22.2/bin/pm2"
 
+cd /opt/test_apps/
 
-mv /opt/pm2_test /opt/test_apps/pm2_test 
+mkdir pm2_test
+
+mv /opt/pm2_test/* /opt/test_apps/pm2_test 
 
 cd /opt/test_apps/pm2_test  
+
 npm install --production
+
+rm -rf /opt/pm2_test
+mkdir /opt/pm2_test
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,11 +3,6 @@
 export NVM_DIR="/root/.nvm"
 export PATH="$NVM_DIR/versions/node/v22.22.2/bin:/usr/local/bin:/usr/bin:/bin"
 
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm  
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-# PM2_CMD=`which pm2`
 PM2_CMD="/root/.nvm/versions/node/v22.22.2/bin/pm2"
 
 
@@ -15,5 +10,3 @@ PM2_CMD="/root/.nvm/versions/node/v22.22.2/bin/pm2"
 cd /opt/test_apps/pm2_test
 
 $PM2_CMD start server.js --name "pm2_test" --output ./output.log  
-
-# $PM2_CMD save
