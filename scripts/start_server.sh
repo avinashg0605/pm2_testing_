04f054d715c11c17be9fa784727c15887ca10ed5
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-05-16 19:46:39 +0530

    appspec change

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,19 +1,3 @@
-# --------------------------------- Start_server ---------------------------------
-
-#!/bin/bash
-
-<<<<<<< HEAD
-# export NVM_DIR="/root/.nvm"
-# export PATH="$NVM_DIR/versions/node/v22.22.2/bin:/usr/local/bin:/usr/bin:/bin"
-
-# PM2_CMD="/root/.nvm/versions/node/v22.22.2/bin/pm2"
-
-# cd /opt/test_apps/pm2_test
-
-# # $PM2_CMD reload pm2_test_1 || \
-# $PM2_CMD start server.js --name "pm2_test_1" --output ./output.log --error ./error.log
-
-
 #!/bin/bash
 APP_NAME="test-apps"
 SOURCE_DIR="/opt/pm2_test"
@@ -39,33 +23,10 @@ cd "$TARGET_DIR"
 npm install --production
 
 # Stop existing PM2 process if running
-"$PM2_CMD" delete "$APP_NAME" >/dev/null 2>&1 || true
+"$PM2_CMD" delete "$APP_NAME" || true
 
 # Start app using PM2
 "$PM2_CMD" start server.js \
     --name "$APP_NAME" \
     --output ./output.log \
     --error ./error.log
-
-# Save PM2 process list
-"$PM2_CMD" save
-
-=======
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-cd $APP_PATH
-
-NODE_VERSION=$(nvm current)
-
-# export NVM_DIR="/root/.nvm"
-# export PATH="$NVM_DIR/versions/node/v22.22.2/bin:/usr/local/bin:/usr/bin:/bin"
-
-# PM2_CMD="/root/.nvm/versions/node/v22.22.2/bin/pm2"
->>>>>>> d759a18d5644a29756fff838a6b5573760501951
-
-
-<<<<<<< HEAD
-=======
-# $PM2_CMD reload pm2_test_1 || \
-nvm exec $NODE_VERSION pm2 start server.js --name "pm2_test_1" --output ./output.log --error ./error.log
->>>>>>> d759a18d5644a29756fff838a6b5573760501951
