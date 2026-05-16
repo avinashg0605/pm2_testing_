d759a18d5644a29756fff838a6b5573760501951
Avinash G <avinashganta65@gmail.com>
2026-05-16 13:27:54 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,12 +2,18 @@
 
 #!/bin/bash
 
-export NVM_DIR="/root/.nvm"
-export PATH="$NVM_DIR/versions/node/v22.22.2/bin:/usr/local/bin:/usr/bin:/bin"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-PM2_CMD="/root/.nvm/versions/node/v22.22.2/bin/pm2"
+cd $APP_PATH
+
+NODE_VERSION=$(nvm current)
+
+# export NVM_DIR="/root/.nvm"
+# export PATH="$NVM_DIR/versions/node/v22.22.2/bin:/usr/local/bin:/usr/bin:/bin"
+
+# PM2_CMD="/root/.nvm/versions/node/v22.22.2/bin/pm2"
 
 cd /opt/test_apps/pm2_test
 
 # $PM2_CMD reload pm2_test_1 || \
-$PM2_CMD start server.js --name "pm2_test_1" --output ./output.log --error ./error.log
+nvm exec $NODE_VERSION pm2 start server.js --name "pm2_test_1" --output ./output.log --error ./error.log
