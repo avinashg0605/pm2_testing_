4cada6a430439ee091c2cd2c1ab373c99a5613da
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-05-18 15:11:05 +0530

    appspec change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end('Hello from Node.js Testing pm2 application from Jenkins 👋\n');
+  res.end('Hello This is Avinash testing Node.js pm2 application from Jenkins 👋\n');
 }).listen(8000, () => {
   console.log('App running on port 8000');
 });
